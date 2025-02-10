f57b7f70a55bc9010d5f498b8e2d9f0973a5bd12
Chamilka Mihiraj <120358764+ChamilkaMihiraj2002@users.noreply.github.com>
2025-02-10 20:03:39 +0530

    Ingredients-crud

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
+import { IngredientsModule } from './ingredients/ingredients.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule],
+  imports: [DatabaseModule, UserModule, IngredientsModule],
   controllers: [AppController],
   providers: [AppService],
 })
