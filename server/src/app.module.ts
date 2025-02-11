929e61285a3efcd29994d5810c46cde563514635
kavindya Abeykoon <132202488+imansika@users.noreply.github.com>
2025-02-11 11:27:58 +0530

    CRUD
    meal crud

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
+import { MealModule } from './meal/meal.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule],
+  imports: [DatabaseModule, UserModule, MealModule],
   controllers: [AppController],
   providers: [AppService],
 })
