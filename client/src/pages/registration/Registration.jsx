61459f4455d78a2475120d3ce67101b8a732560f
Anjana Edirisinghe <anjanaape@gmail.com>
2025-02-13 01:10:31 +0530

    ts to js

---
+++ client/src/pages/registration/Registration.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Register(){
+
+    return(
+        <div>
+            <div>
+                
+
+            </div>
+        </div>
+    )
+}
+
+export default Register;
