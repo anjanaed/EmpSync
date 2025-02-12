fe166425c633b5ec0f44055d5cb990952ff5e508
Anjana Edirisinghe <anjanaape@gmail.com>
2025-02-13 02:27:41 +0530

    register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Navbar from './components/hrDashboard/hrNavbar'
 import './index.css'
+import Register from './pages/registration/Registration'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Navbar/>
+      <Register/>
     </>
   )
 }
--- client/src/pages/registration/Registration.jsx
+++ client/src/pages/registration/Registration.jsx
@@ -1,16 +1,47 @@
-import React from 'react';
+import React from "react";
+import styles from "./Registration.module.css";
+import Navbar from "../../components/hrDashboard/hrNavbar";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fa1,fa2 } from '@fortawesome/free-solid-svg-icons';
 
-
-function Register(){
-
-    return(
-        <div>
-            <div>
-                
-
+function Register() {
+  return (
+    <div className={styles.home}>
+      <div className={styles.homeNavbar}>
+        <Navbar />
+      </div>
+      <div className={styles.homeContent}>
+        <div className={styles.rBox}>
+            <div className={styles.rTable}>
+            <div className={styles.rTableContentSelected}>1 User Profile</div>
+            <div className={styles.rTableContent}>2 Bio Data</div>
             </div>
+
+
+        <div className={styles.mid}>
+          <h1>Register New Employees</h1>
+          <span>
+            Enter User Information & Provide Fingerprint At The Next Page To Add New
+            Employee To The System
+          </span>
         </div>
-    )
+        <div className={styles.inputs}>
+            <div className={styles.inputsBox}>
+            <label>Employee Id</label><br></br>  
+            <input type="textbox"></input>
+            </div>
+            <div className={styles.inputsBox}>
+            <label>Full Name</label><br></br>  
+            <input type="textbox"></input>
+            </div>
+
+        </div>
+
+        </div>
+        
+      </div>
+    </div>
+  );
 }
 
 export default Register;
