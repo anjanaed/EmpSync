9ca5a52bb20669d17cfa469e6d232d6614eeda76
Anjana Edirisinghe <anjanaape@gmail.com>
2025-02-15 22:04:58 +0530

    Add logo asset, update navbar with logo, and implement loading and button components

--- client/src/components/hrDashboard/hrNavbar.jsx
+++ client/src/components/hrDashboard/hrNavbar.jsx
@@ -1,32 +1,71 @@
-import  React  from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUsers,faUserPlus,faFileInvoice,faHand, faDollarSign,faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
-import {Link} from 'react-router-dom'
-import styles from './hrNavbar.module.css';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUsers,
+  faUserPlus,
+  faFileInvoice,
+  faHand,
+  faDollarSign,
+  faArrowRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import styles from "./hrNavbar.module.css";
+import img from "../../assets/logo.png.png";
 
-
-function Navbar(){
-
-
-
-    return(
-        <div>
-        <div className={styles.navbar_base}>
-          <div className={styles.navbar}>
-          <div className={styles.navbarItems}><FontAwesomeIcon icon={faUsers} /> Employees</div>
-          <div className={styles.navbarItems}> <FontAwesomeIcon icon={faUserPlus} /> Registration</div>
-          <div className={styles.navbarItems}> <FontAwesomeIcon icon={faDollarSign} /> Payroll</div>
-          <div className={styles.navbarItems}> <FontAwesomeIcon icon={faHand} /> Leave Apply</div>
-          <div className={styles.navbarItems}> <FontAwesomeIcon icon={faFileInvoice} /> Reports</div>
+function Navbar({ selected }) {
+  return (
+    <div>
+      <div className={styles.navbar_base}>
+        <img className={styles.logo} src={img}></img>
+        <div className={styles.navbar}>
+          <div
+            className={
+              selected === "E" ? styles.navbarItemsSelected : styles.navbarItems
+            }
+          >
+            <FontAwesomeIcon icon={faUsers} /> Employees
           </div>
-          <div className={styles.logout}>
-            <FontAwesomeIcon icon={faArrowRightFromBracket}/> Log Out
+          <div
+            className={
+              selected === "R" ? styles.navbarItemsSelected : styles.navbarItems
+            }
+          >
+            {" "}
+            <FontAwesomeIcon icon={faUserPlus} /> Registration
+          </div>
+          <div
+            className={
+              selected === "P" ? styles.navbarItemsSelected : styles.navbarItems
+            }
+          >
+            {" "}
+            <FontAwesomeIcon icon={faDollarSign} /> Payroll
+          </div>
+          <div
+            className={
+              selected === "L" ? styles.navbarItemsSelected : styles.navbarItems
+            }
+          >
+            {" "}
+            <FontAwesomeIcon icon={faHand} /> Leave Apply
+          </div>
+          <div
+            className={
+              selected === "Rep"
+                ? styles.navbarItemsSelected
+                : styles.navbarItems
+            }
+          >
+            {" "}
+            <FontAwesomeIcon icon={faFileInvoice} /> Reports
           </div>
         </div>
+        <div className={styles.logout}>
+          <FontAwesomeIcon icon={faArrowRightFromBracket} /> Log Out
+        </div>
+      </div>
     </div>
-    )
-
-    
+  );
 }
 
 export default Navbar;
