ec4336bbbeb6808ea49955a9643f7d14efe35e4b
Dishma Gangulel <madushankaaldg.22@uom.lk>
2025-02-27 18:49:55 +0530

    Login page style

---
+++ client/src/pages/Login/Login.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Form, Input,Button, Checkbox } from "antd";
+import "./Login.css";
+import illustration from "../../assets/illustration.png";
+
+const LoginPage = () => {
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        {/* Left Section */}
+        <div className="login-left">
+          
+          <img
+            src={illustration}
+            alt="Illustration"
+            className="login-illustration"
+          />
+        </div>
+
+        {/* Right Section */}
+        <div className="login-right">
+          <h2 className="login-title">WELCOME BACK</h2>
+          <p className="login-subtitle">Please enter your details.</p>
+          
+          <Form layout="vertical" className="login-form">
+            <Form.Item label="Employee ID" name="employeeID" rules={[{ required: true, message: 'Please input your Employee ID!' }]}> 
+              <Input placeholder="Enter your Employee ID" />
+            </Form.Item>
+            
+            <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Please input your password!' }]}> 
+              <Input.Password placeholder="**********" />
+            </Form.Item>
+            
+            <div className="login-options">
+              <Checkbox>Remember me</Checkbox>
+              <a href="#" className="forgot-password">Forgot password</a>
+            </div>
+            
+            <Form.Item>
+              <button type="submit" className="login-button">Login</button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
