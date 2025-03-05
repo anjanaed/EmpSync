a045bbddc394cc02c8735d88e5dd8b968809ba91
Dishma Gangulel <madushankaaldg.22@uom.lk>
2025-03-05 18:33:14 +0530

    Refactor LoginRole component to include role selection and enhance styling; update CSS for improved layout and readability

--- client/src/pages/LoginRole/LoginRole.jsx
+++ client/src/pages/LoginRole/LoginRole.jsx
@@ -1,13 +1,18 @@
-import React from "react";
-import { Form, Input, Checkbox } from "antd";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./LoginRole.module.css";
 import illustration from "../../assets/illustration2.png";
 import { UserOutlined } from '@ant-design/icons';
-import { Avatar, Space } from 'antd';
+import { Radio, Avatar } from "antd";
 
 const LoginPage = () => {
   const navigate = useNavigate();
+  const [value, setValue] = useState(null);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
   const handleLogin = () => {
     navigate("/reg"); 
   };
@@ -26,14 +31,27 @@ const LoginPage = () => {
           <h2 className={styles.loginTitle}>Welcome Back !</h2>
           <div className={styles.logindetails}>             
               <div className={styles.loginavatar}>
-                 <Avatar size={55} icon={<UserOutlined />} />
+                 <Avatar 
+                   size={55} 
+                   icon={<UserOutlined />} 
+                   style={{ backgroundColor: '#8b0000', color: '#fff' }} 
+                 />
               </div>
               <div className={styles.loginname}>
                 <p className={styles.username}>Mr.John Doe</p>
                 <p className={styles.userpost}>HR Manager</p>
               </div>
           </div>
-          <p className={styles.loginSubtitle}>Please Enter Your Credentials.</p>
+          <p className={styles.loginSubtitle}>Login As: </p>
+          <hr className={styles.divider} />
+          <Radio.Group value={value} onChange={handleChange} className={styles.radioGroup}>
+              <Radio.Button value="employee" className={styles.customRadio}>
+                Employee
+              </Radio.Button>
+              <Radio.Button value="inventory_manager" className={styles.customRadio}>
+                Inventory Manager
+              </Radio.Button>
+          </Radio.Group>
           <button type="submit" className={styles.loginButton} onClick={handleLogin}>Login</button>
         </div>
       </div>
