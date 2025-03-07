ec3e1d853d6ba4954107f201a280403b31211533
Hashan Rajakaruna <hashanrajakaruna123456@gmail.com>
2025-03-07 17:19:43 +0530

    add current date and time display to OrderTab component and update styles

--- client/src/pages/OrderTab/OrderTab.jsx
+++ client/src/pages/OrderTab/OrderTab.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Carousel } from 'antd';
 import { Typography } from 'antd';
 import styles from './OrderTab.module.css';
@@ -14,21 +14,42 @@ const OrderTab = () => {
         const currentHour = new Date().getHours();
         if (currentHour < 12) {
             return  "Good Morning සුභ උදෑසනක් වේවා காலை வணக்கம்";
-        } else if (currentHour < 18) {
+        } else if (currentHour < 17) {
             return "Good Afternoon සුභ දහවලක් වේවා மதிய வணக்கம்";
         } else {
             return "Good Evening සුභ සන්ධ්‍යාවක් වේවා மலை வணக்கம்";
         }
     };
 
+    const getCurrentDateTime = () => {
+        const now = new Date();
+        const options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' };
+        const timeOptions = { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true };
+        const date = now.toLocaleDateString('en-US', options);
+        const time = now.toLocaleTimeString('en-US', timeOptions);
+        return `${date} | ${time}`;
+    };
+
+    const [currentDateTime, setCurrentDateTime] = useState(getCurrentDateTime());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentDateTime(getCurrentDateTime());
+        }, 1000);
+        return () => clearInterval(intervalId);
+    }, []);
+
     return (
         <>
+            
             
             <Carousel ref={carouselRef} infinite={false} dots={true}>
                 <div>
                     <div className={styles.contentStyle1}>
                         <br />
                         <div><Typography.Title level={2} className={styles.getGreeting}>{getGreeting() }</Typography.Title></div>
+                        <div><Typography.Title level={1} className={styles.mainTitle}>Welcome to Helix Food Ordering</Typography.Title></div>
+                        <div><Typography.Title level={2} className={styles.getGreeting}>{currentDateTime}</Typography.Title></div>
                         <button onClick={next}>Move 1 to 2</button>
                     </div>
                 </div>
