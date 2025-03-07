fbc0a6094e71a814adc1e6fcc2368da28f53b202
Hashan Rajakaruna <hashanrajakaruna123456@gmail.com>
2025-03-07 12:52:29 +0530

    add ReOrder component and VSCode settings

---
+++ client/src/components/ReOrder/ReOrder.jsx
@@ -0,0 +1,11 @@
+const ReOrder = () => {
+    return (
+        <div>
+            <h1>This is ReOrder Component.</h1>
+        </div>
+    );
+}
+
+export default ReOrder;
+
+//look at how to set props in the component
