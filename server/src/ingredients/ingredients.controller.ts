89368ff827d6e8e7fbf8b1649ee8e7d9acb72281
Chamilka Mihiraj <120358764+ChamilkaMihiraj2002@users.noreply.github.com>
2025-03-29 23:24:52 +0530

    Update ingredients.controller.ts

--- server/src/ingredients/ingredients.controller.ts
+++ server/src/ingredients/ingredients.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpException, HttpStatus } from '@nestjs/common';
 import { IngredientsService } from './ingredients.service';
 import { Prisma } from '@prisma/client';
 import { threadId } from 'worker_threads';
@@ -17,21 +17,21 @@ export class IngredientsController {
     return this.ingredientsService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ingredientsService.findOne(id);
-  }
-
   @Get('price/low')
   async findLowPriceIngredients() {
     console.log('Received request for low price ingredients');
     return this.ingredientsService.findLowPriceIngredients();
+  }Z
+
+  @Get('price/comparison')
+  async getPriceComparison() {
+    console.log('Received request for price comparison');
+    return this.ingredientsService.getPriceComparison();
   }
 
-  @Get('price/high')
-  async findHighPriceIngredients() {
-    console.log('Received request for high price ingredients');
-    return this.ingredientsService.findHighPriceIngredients();
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.ingredientsService.findOne(id);
   }
 
   @Patch(':id')
