a4d0272eee887bf8dd96d61ea0e0e823c6420271
Anjana Edirisinghe <anjanaape@gmail.com>
2025-04-23 23:04:24 +0530

    Fully customizable NavBar

--- client/src/components/pages/HR/EmployeePage/EmployeePage.jsx
+++ client/src/components/pages/HR/EmployeePage/EmployeePage.jsx
@@ -1,13 +1,54 @@
-import React from "react";
+import React from 'react'
+import {
+  faUsers,
+  faUserPlus,
+  faFileInvoice,
+  faDollarSign,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Employees from "../../../organisms/EmployeeList/Employees";
-import NavBar from "../../../organisms/NavBar/NavBar";
+import NavBar from '../../../organisms/NavBar/NavBar';
+
 
 const EmployeePage = () => {
   return (
-    <>
-      <NavBar Comp={Employees} />
-    </>
-  );
-};
+    <NavBar
+    titleLines={["Human", "Resource", "Management"]}
+    menuItems={[
+      {
+        key: "1",
+        icon: <FontAwesomeIcon icon={faUsers} />,
+        label: "Employees",
+        link: "/"
+      },
+      {
+        key: "2",
+        icon: <FontAwesomeIcon icon={faUserPlus} />,
+        label: "Registration",
+        link: "/reg"
+      },
+      {
+        key: "3",
+        icon: <FontAwesomeIcon icon={faDollarSign} />,
+        label: "Payrolls",
+        link: "/payroll"
+      },
+      {
+        key: "4",
+        icon: <FontAwesomeIcon icon={faFileInvoice} />,
+        label: "Reports",
+        link: "/reportPage"
+      }
+    ]}
+    Comp={Employees}
+  />
+    
+    
+  )
+}
 
-export default EmployeePage;
+export default EmployeePage
+
+
+
+
--- client/src/components/pages/HR/ReportPage/ReportPage.jsx
+++ client/src/components/pages/HR/ReportPage/ReportPage.jsx
@@ -1,14 +1,54 @@
 import React from 'react'
-import ReportDashboard from '../../../organisms/ReportDashboard/ReportDashboard';
+import {
+  faUsers,
+  faUserPlus,
+  faFileInvoice,
+  faDollarSign,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import NavBar from '../../../organisms/NavBar/NavBar';
+import ReportDashboard from '../../../organisms/ReportDashboard/ReportDashboard';
 
-const ReportPage=()=>{
 
-    return(<>
-    <NavBar Comp={ReportDashboard}/>
-    </>
-
-    )
-};
+const ReportPage = () => {
+  return (
+    <NavBar
+    titleLines={["Human", "Resource", "Management"]}
+    menuItems={[
+      {
+        key: "1",
+        icon: <FontAwesomeIcon icon={faUsers} />,
+        label: "Employees",
+        link: "/"
+      },
+      {
+        key: "2",
+        icon: <FontAwesomeIcon icon={faUserPlus} />,
+        label: "Registration",
+        link: "/reg"
+      },
+      {
+        key: "3",
+        icon: <FontAwesomeIcon icon={faDollarSign} />,
+        label: "Payrolls",
+        link: "/payroll"
+      },
+      {
+        key: "4",
+        icon: <FontAwesomeIcon icon={faFileInvoice} />,
+        label: "Reports",
+        link: "/reportPage"
+      }
+    ]}
+    Comp={ReportDashboard}
+  />
+    
+    
+  )
+}
 
 export default ReportPage
+
+
+
+
