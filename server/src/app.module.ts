9018a7568822f1e752d11f5f8ef9dd478b90355e
Anjana Edirisinghe <anjanaape@gmail.com>
2025-05-06 21:07:00 +0530

    feat: Enhance budget management with fetching and updating functionality, and improve registration error handling

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -15,6 +15,7 @@ import { PayeTaxModule } from './PayeTaxSlab/PayeTax.module';
 import { AuthModule } from './auth/auth.module';
 import { ScheduleModule as NestScheduleModule } from '@nestjs/schedule';
 import { ReportModule } from './Reports/reports.module';
+import { BudgetsModule } from './Budgets/Budgets.module';
 
 
 
@@ -33,6 +34,7 @@ import { ReportModule } from './Reports/reports.module';
     PayrollModule,
     AttendanceModule,
     OrdersModule,
+    BudgetsModule,
     ScheduleModule, 
     AdjustmentModule,
     ReportModule,
