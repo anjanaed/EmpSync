d86ba7b7809b674c7283780f3c9081d14ba562fa
Chamilka Mihiraj <120358764+ChamilkaMihiraj2002@users.noreply.github.com>
2025-05-07 12:55:34 +0530

    Update EmployeeDetails.jsx

--- client/src/components/pages/HR/Attendance/EmployeeDetails.jsx
+++ client/src/components/pages/HR/Attendance/EmployeeDetails.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Employee from "../../../organisms/Attendance/Employee Details/EmployeeDetails"; // Corrected import path
+// import Employee from "../../../organisms/Attendance/Employee Details/EmployeeDetails"; // Corrected import path
 import NavBar from "../../../organisms/NavBar/NavBar";
 
 const EmployeeDetails = () => {
   return (
     <>
-      <NavBar Comp={Employee} />
+      <NavBar />
     </>
   );
 };
