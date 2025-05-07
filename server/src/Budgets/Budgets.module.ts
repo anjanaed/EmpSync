84b28b66aec2f1f41743b0b0351d4bcda2e4fa1e
Chamilka Mihiraj <120358764+ChamilkaMihiraj2002@users.noreply.github.com>
2025-05-07 19:29:27 +0530

    Delete Budgets.module.ts

--- server/src/Budgets/Budgets.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BudgetService } from './Budgets.service';
-import { BudgetsController } from './Budgets.controller';
-
-@Module({
-  controllers: [BudgetsController],
-  providers: [BudgetService],
-})
-export class BudgetsModule {}
