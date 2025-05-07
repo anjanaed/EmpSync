d3c2945bf00405b2829ae06e3e3b2fefa5ec31e1
Dishma Gangulel <madushankaaldg.22@uom.lk>
2025-05-07 14:08:07 +0530

    feat: Refactor Cart component and integrate meal ordering functionality

--- client/src/components/pages/UserAccount/MealsPage/MealsPage.jsx
+++ client/src/components/pages/UserAccount/MealsPage/MealsPage.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import NavBar from "../../../organisms/UserPortal/ResponsiveNavbar/ResponsiveNav";
+import Cart from "../../../organisms/UserPortal/Meals/Meals"; // Import Cart component
 
-const ProfilePage = () => {
+const MealsPage = () => {
   return (
     <>
       <NavBar />
-      <div>hello </div>
+      <div>hello</div>
+      <Cart
+        items={[
+          { id: 1, name: "Sneakers", price: 59.99, quantity: 2 },
+          { id: 2, name: "Boots", price: 89.99, quantity: 1 },
+        ]}
+        onRemove={(id) => console.log("Remove item with id:", id)}
+        onCheckout={() => console.log("Proceed to checkout")}
+      />
     </>
   );
 };
 
-export default ProfilePage;
+export default MealsPage;
