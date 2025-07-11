b869ad2fe33b768d3eb82f4ae2941b7b1c7c4b73
Chamilka Mihiraj <120358764+ChamilkaMihiraj2002@users.noreply.github.com>
2025-07-11 15:13:05 +0530

    Update regis.ts

--- server/src/core/super-admin-auth/regis.ts
+++ server/src/core/super-admin-auth/regis.ts
@@ -3,10 +3,10 @@ import axios from "axios";
 // === CONFIGURE THESE VALUES ===
 const auth0Url = "dev-77pr5yqzs0m53x77.us.auth0.com"; // e.g. your-tenant.auth0.com
 const auth0ClientId = "RsxaQQdCDS1kk422wUEJtql0FY9AGFuN";
-const email = "anjanaape2002@gmail.com";
-const username = "desiredUsername";
-const password = "StrongPassword123!";
-const id = "yourCustomId"; // If you want to use this as username
+const email = "chamilka2002@gmail.com";
+const username = "superadminTest"; // Use this if you want to register with a username
+const password = "chamilka2002.";
+const id = "e20020705"; // If you want to use this as username
 
 const signUpUser = async () => {
   try {
