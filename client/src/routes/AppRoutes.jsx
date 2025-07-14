58c7a19b1600420ac3b4db52ff01c3e3e87ca732
Chamilka Mihiraj <120358764+ChamilkaMihiraj2002@users.noreply.github.com>
2025-07-14 17:39:16 +0530

    Update AppRoutes.jsx

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -3,11 +3,7 @@ import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 import PasswordReset from "../components/pages/Login/PasswordReset/PasswordReset";
 import FingerPrintPage from '../components/pages/HR/FingerPrintPage/FingerPrintPage';
 import OrderTab from "../components/pages/OrderTab/OrderTab";
-import SuperAdmin from "../components/pages/SuperAdmin/SuperAdmin";
-import SuperAdminLogin from '../components/pages/Login/SuperAdmin/LoginPage/Login';
-import Organizations from '../components/organisms/SuperAdmin/pages/Organizations/Organization List/OrganizationList';
-import Roles from '../components/organisms/SuperAdmin/pages/Roles/RolesList';
-import Permissions from '../components/organisms/SuperAdmin/pages/Permissions/PermissionsList';
+
 import UserFingerPrintRegister from '../components/organisms/OrderTabUI/UserFingerPrintRegister/UserFingerPrintRegister';
 import MealPage03 from '../components/organisms/UserPortal/MealPage03/MealPage03';
 // import KitchenAdmin from "../components/pages/KitchenAdmin/SchedulePage/KitchenAdmin";
@@ -31,12 +27,6 @@ const AppRoutes = () => (
           {/* Public Routes */}
           <Route path="/passwordReset" element={<PasswordReset />} />
           <Route path="/OrderTab" element={<OrderTab />} />
-          <Route path="/SuperAdmin/login" element={<SuperAdminLogin />} />
-          <Route path="/SuperAdmin/dashboard" element={<SuperAdmin />}>
-            <Route path="organizations" element={<Organizations />} />
-            <Route path="roles" element={<Roles />} />
-            <Route path="permissions" element={<Permissions />} />
-          </Route>
           <Route path="/user-fingerprint-register" element={<UserFingerPrintRegister />} />
           <Route path="/MealPage03" element={<MealPage03 />} />
           {/* <Route path="/kitchen-admin" element={<KitchenAdmin />} />
