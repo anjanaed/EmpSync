da90a0879704a86040527af62d2c5ed799c9f68b
Anjana Edirisinghe <anjanaape@gmail.com>
2025-07-21 13:46:46 +0530

    loading fixes

--- client/src/components/organisms/Kitchen/Schedule/Calendar.jsx
+++ client/src/components/organisms/Kitchen/Schedule/Calendar.jsx
@@ -807,6 +807,12 @@ const MealPlanner = () => {
     return null;
   };
 
+
+  if (isUpdateLoading){
+    return <Loading  />;
+
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
